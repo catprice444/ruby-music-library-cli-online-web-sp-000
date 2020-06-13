6ae120605604ff390b75b1f25d74fb1cfadbe171
Catherine Price <catprice444@gmail.com>
2020-06-13 16:43:24 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -43,7 +43,7 @@ class MusicLibraryController
 
   def list_artists
     Artist.all.sort{|a, b| a.name <=> b.name}.each_with_index do |a, i|
-      puts "#{i+1}. #{a.artist, a.name, a.genre}"
+      puts "#{i+1}. #{a.name}"
     end
   end
 
