cc19c68acd931bc33a8928cb2bc037f18e48654a
Catherine Price <catprice444@gmail.com>
2020-06-13 15:07:58 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -13,14 +13,6 @@ class Song
     @@all
   end
 
-  # def self.find_by_name(name)
-  #   self.all.detect {|song| song.name == name}
-  # end
-  #
-  # def self.find_or_create_by_name(name)
-  #   self.find_by_name(name) || self.create(name)
-  # end
-
   def genre
     @genre
   end
