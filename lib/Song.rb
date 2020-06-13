571a1d40514f46f9cedaa7e0af5bf5c2569e081d
Catherine Price <catprice444@gmail.com>
2020-06-13 13:40:43 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -2,9 +2,11 @@ class Song
   attr_accessor :name, :artist, :genre
   @@all = []
 
-  def initialize(name, artist= nil, genre= nil)
+  def initialize(name, artist=nil, genre=nil)
     @name = name
-    
+    self.artist=(artist) if artist !=nil
+    self.genre=(genre) if genre !=nil
+
   end
 
   def self.all
