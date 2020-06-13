25bd2cfdd316e212289db7ecf7b28601ff66f427
Catherine Price <catprice444@gmail.com>
2020-06-13 15:02:48 +0000

    Automatically backed up by Learn

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -5,5 +5,11 @@ class MusicImporter
     @path = path
   end
 
-  
+ def files
+   Dir.entries(@path).select! {|file| file.end_with?(".mp3")}
+  #  loads all the MP3 files in the path directory
+    # normalizes the filename to just the MP3 filename with no path
+ end
+
+
 end
