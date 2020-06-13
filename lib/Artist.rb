9daeae50c055e0f728bd307e46936ff627c960d3
Catherine Price <catprice444@gmail.com>
2020-06-13 13:16:44 +0000

    Automatically backed up by Learn

--- lib/Artist.rb
+++ lib/Artist.rb
@@ -1,2 +1,25 @@
 class Artist
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    # self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def destroy_all
+    @@all.clear
+  end
+
+  def create
+
+  end
 end 
