a1388535609a32ca44413ff9945c3fa894881fe1
Catherine Price <catprice444@gmail.com>
2020-06-13 17:15:19 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -82,7 +82,7 @@ class MusicLibraryController
     songs = Song.all
     if (1..song.length).include?(input)
       song = Song.all.sort{|a, b| a.name <=> b.name}[input - 1]
-    end 
+    end
     puts "Playing #{song.name} by #{song.artist.name}"  
 
   end
