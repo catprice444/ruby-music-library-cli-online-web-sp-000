ea37056893c13fd4ecce9be4840b07478b198980
Catherine Price <catprice444@gmail.com>
2020-06-13 16:37:01 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -37,7 +37,7 @@ class MusicLibraryController
 
   def list_songs
     Songs.all.sort{|a, b| a.name <=> b.name}.each_with_index do |a, i, g|
-      puts "#{i+1}. #{a.name} #{g.genre}"
+      puts "#{i+1}. #{a}"
     end
   end
 
