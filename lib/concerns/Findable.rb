d6ed19963ead2c2ed24bfe47cae07ef0c80f2057
Catherine Price <catprice444@gmail.com>
2020-06-13 14:41:34 +0000

    Automatically backed up by Learn

--- lib/concerns/Findable.rb
+++ lib/concerns/Findable.rb
@@ -1,4 +1,5 @@
 module Concerns::Findable
+  
   def self.find_by_name(name)
     self.all.detect {|song| song.name == name}
   end
@@ -6,4 +7,5 @@ module Concerns::Findable
   def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create(name)
   end
+  
 end 
